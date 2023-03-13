646a411346d9cf9c42db63f2afc12076a13dbc92
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-13 16:36:02 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int minNumber = 1;
+            int maxNumber = 100;
+            int number = 2;
+            int numberRandom;
+            int degreeNumber = 0;
+            int degree = 1;
+
+            numberRandom = random.Next(minNumber, maxNumber + 1);
+
+            while (numberRandom >= degree)
+            {
+                degreeNumber++;
+                degree *= number;
+            }
+
+            Console.WriteLine($"Рандомное число {numberRandom} будет {number} в степени {degreeNumber} то есть {degree} которое больше рандомного числа {numberRandom} ");
+        }
+    }
+}
